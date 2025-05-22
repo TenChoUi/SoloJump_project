4439bd3888c1fb5975c6338929153b4b1591a3ef
TenChoU <fgd5463@gmail.com>
2025-05-22 15:39:16 +0900

    [fix] player fix
    [fix] player fix

--- Assets/Scripts/JumpPlate.cs
+++ Assets/Scripts/JumpPlate.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class JumpPlate : MonoBehaviour
 {
     public GameObject spring;
-    public float jumpForce = 500f;
+    public float jumpForce = 200f;
 
     private void OnTriggerEnter(Collider other)
     {
         Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
         if (rb != null)
         {
-            //rb.velocity = Vector3.zero;  //떨어지는 힘을 상쇄
+            //rb.velocity = Vector3.zero;  // force = 0
             rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
